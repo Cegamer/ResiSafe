3f43c192b971595de08070eb7148d602eab023ba
Cegamer <cesarmauriciomn2012@gmail.com>
2024-04-30 19:19:39 -0500

    Lista de conjuntos

--- AppConjuntos/app/src/main/java/com/resisafe/appconjuntos/ui/AppMaster/AppmasterListaConjuntosFragment.kt
+++ AppConjuntos/app/src/main/java/com/resisafe/appconjuntos/ui/AppMaster/AppmasterListaConjuntosFragment.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -54,6 +55,8 @@ class AppmasterListaConjuntosFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val apiService = RetrofitClient.apiService
         val context = this.requireContext()
+        activity?.actionBar?.title = "Conjuntos Registrados"
+
 
         val boton = view.findViewById<Button>(R.id.buttonNuevoConjunto)
         boton.setOnClickListener() {
